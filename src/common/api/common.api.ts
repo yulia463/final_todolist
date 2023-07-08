31abd712f9b72df89ab0d44d44b92b7dc8f4faa6
yuliaslatvitskaya <yuli.slat@yandex.ru>
2023-07-08 15:25:04 +0200

    friday : todo 4

--- src/common/api/common.api.ts
+++ src/common/api/common.api.ts
@@ -4,7 +4,7 @@ export const instance = axios.create({
 	baseURL: 'https://social-network.samuraijs.com/api/1.1/',
 	withCredentials: true,
 	headers: {
-		'API-KEY': '1cdd9f77-c60e-4af5-b194-659e4ebd5d41'
+		'API-KEY': 'f366fe68-e2f7-4b66-9e39-54a117a8eccb'
 	}
 })
 
